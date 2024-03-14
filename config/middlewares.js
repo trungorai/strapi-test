83991f53d483955a5e3443bdc5298094da54adef
trungbach <trunga2k29@gmail.com>
2024-03-14 13:57:13 +0700

    chore: add cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,19 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::poweredBy",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: env("CORS_ORIGINS").split(","),
+    },
+  },
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
