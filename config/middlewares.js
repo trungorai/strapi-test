de069ae692a70c28cfd0cc708ae8807cfb0513a8
trungbach <trunga2k29@gmail.com>
2024-03-14 15:37:34 +0700

    chore: temp allow cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,23 +3,24 @@ module.exports = ({ env }) => [
   "strapi::errors",
   "strapi::security",
   "strapi::poweredBy",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      headers: "*",
-      origin: env("CORS_ORIGINS")
-        ? env("CORS_ORIGINS").split(",")
-        : [
-            "http://localhost:1337",
-            "https://oraidex.io",
-            "https://develop-v3.oraiswap-frontend.pages.dev",
-            "http://localhost:3000",
-            "https://oraidex-landingpage.pages.dev",
-            "https://fresh-harmony-bf37524082.strapiapp.com",
-          ],
-    },
-  },
+  "strapi::cors",
+  // {
+  //   name: "strapi::cors",
+  //   config: {
+  //     enabled: true,
+  //     headers: "*",
+  //     origin: env("CORS_ORIGINS")
+  //       ? env("CORS_ORIGINS").split(",")
+  //       : [
+  //           "http://localhost:1337",
+  //           "https://oraidex.io",
+  //           "https://develop-v3.oraiswap-frontend.pages.dev",
+  //           "http://localhost:3000",
+  //           "https://oraidex-landingpage.pages.dev",
+  //           "https://fresh-harmony-bf37524082.strapiapp.com",
+  //         ],
+  //   },
+  // },
   "strapi::query",
   "strapi::body",
   "strapi::session",
